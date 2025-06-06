da18f71bb8d5ad99acd061d1c1d47f2321eb9b66
Nathan Hill <nathan.hill@mfg.at>
2025-06-06 11:08:42 +0200

    final commit - entire project without ai

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const container = document.querySelector('.drawpad');
 
-createGrid(90);
+
+
+  function genBckCol() {
+    return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+}
 
 function createGrid(cols) {
     const rows = cols;
@@ -12,6 +16,29 @@ function createGrid(cols) {
       cell.classList.add('box');
     }
     container.style.setProperty(`grid-template-columns`, `repeat(${cols}, 1fr)`);
-  }
+
+    let cells = document.querySelectorAll('.box'); 
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = genBckCol();
+      }); 
+    });
+}
+
+function resetGrid() {
+    let cells = document.querySelectorAll('.box'); 
+    cells.forEach(cell => {
+        cell.style.backgroundColor = 'rgb(255,255,255)';
+    }); 
+
+    var userGridInput = parseInt(prompt("Enter a Value between 1 and 90", "1"));
+    createGrid(userGridInput);
+}
+
+createGrid(10);
+
+
   
+
+
 
